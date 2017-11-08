5bd8f25d0703b208ff1899a6423a2ad473863a3e
Kim Foukaris <kimfoukaris@outlook.com>
2017-11-08 14:56:28 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,8 +24,8 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
     if (position_1 == "X" && position_2 == "X" and position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-      win = [position_1, position_2, position_3]
     end
+    win = [position_1, position_2, position_3]
   end
 end
 
